abbd02b9dd4cd736a152cc6f70c84616c6ffe1cc
saba <hesam@ignite.ee>
2015-10-31 13:59:01 -0500

    added twitter button support

--- scripts/customjs.js
+++ scripts/customjs.js
@@ -17,7 +17,7 @@ function getQuoteFromAndruxnetAPI(){
 	function showQuote(result){
 		$('#quote_text').html(result.quote);
 		$('#quote_author').html(result.author);
-		console.log(result.content);
+		generateTweetLink(result.quote);
 	}
 	$.ajax({
 	  url : 'https://andruxnet-random-famous-quotes.p.mashape.com/cat={famous}',
@@ -26,4 +26,15 @@ function getQuoteFromAndruxnetAPI(){
 	  headers : {"X-Mashape-Key":"JvYI0i6ZtnmshKGpIjC37Js1jNArp1AANEDjsnRmZ4yxiVu0Re","Content-Type":"application/x-www-form-urlencoded","Accept":"application/json"},
 	  success: showQuote
 	});
+}
+function generateTweetLink(quote){
+    var link = "https://twitter.com/intent/tweet?text=";
+    //var text = quote.replace(/(<([^>]+)>)/gi, "");
+    var shortQuote = quote.substring(0, 99);
+    link += shortQuote + "...";
+    link = encodeURI(link);
+    link += "&url=http%3A%2F%2Fsabahang.github.io%2Fquote-generator%2F&via=SabaAhang"
+    var linkElement = '<a class="twitter-share-button" href="' + link + '" data-size="large" align="left">Tweet</a>'
+    $('#twitter').children().replaceWith(linkElement);
+    twttr.widgets.load();
 }
