0f989544285dc000706f3f2c7fbb4830753ac8ae
saba <hesam@ignite.ee>
2015-10-30 22:55:19 -0500

    added customjs with ajax call methods and bound to buttons

---
+++ scripts/customjs.js
@@ -0,0 +1,30 @@
+
+ $(document).ready( function() {
+     $('#wisdom_api_button').click(getQuoteFromWisdomAPI);
+     $('#famous_quote_button').click(getQuoteFromAndruxnetAPI);
+ });
+
+//https://github.com/gophergala/wisdom
+function getQuoteFromWisdomAPI(){
+	function showQuote(result){
+		$('#quote_text').html(result.content);
+		$('#quote_author').html(result.author.name);
+		console.log(result.content);
+	}
+	$.getJSON('https://wisdomapi.herokuapp.com/v1/random', showQuote);
+}
+//https://market.mashape.com/andruxnet/random-famous-quotes
+function getQuoteFromAndruxnetAPI(){
+	function showQuote(result){
+		$('#quote_text').html(result.quote);
+		$('#quote_author').html(result.author);
+		console.log(result.content);
+	}
+	$.ajax({
+	  url : 'https://andruxnet-random-famous-quotes.p.mashape.com/cat={famous}',
+	  type: 'POST',
+	  dataType : "json",
+	  headers : {"X-Mashape-Key":"JvYI0i6ZtnmshKGpIjC37Js1jNArp1AANEDjsnRmZ4yxiVu0Re","Content-Type":"application/x-www-form-urlencoded","Accept":"application/json"},
+	  success: showQuote
+	});
+}
